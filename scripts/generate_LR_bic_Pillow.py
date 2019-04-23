e85a6093bce5eb22bedaf5a0ce02b16ead89a30a
Tai <lythanhtaiyoung666@gmail.com>
2019-04-23 17:28:39 +0700

    fix path error

--- scripts/generate_LR_bic_Pillow.py
+++ scripts/generate_LR_bic_Pillow.py
@@ -11,8 +11,8 @@ from utils.progress_bar import ProgressBar  # noqa: E402
 
 
 # LR, HR path
-hr_path = "content/dataset/img_align_celeba_png_set_1_HR/*"
-lr_path = "content/dataset/img_align_celeba_png_set_1_LR/"
+hr_path = "/content/dataset/img_align_celeba_png_set_1_HR/*"
+lr_path = "/content/dataset/img_align_celeba_png_set_1_LR/"
 hr_list = sorted(glob(hr_path))
 
 # Scale factor
