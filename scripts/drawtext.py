1483b5527b9a80538c88f1791f9fb7d8b6ed028a
Tai <lythanhtaiyoung666@gmail.com>
2019-04-23 15:35:40 +0700

    add drawtext on image

--- scripts/drawtext.py
+++ scripts/drawtext.py
@@ -1,7 +1,7 @@
 from PIL import ImageDraw
 import cv2
 
-img = cv2.imread("./")
+img = cv2.imread("C:/Users/LTT/OneDrive - K3v/Documents/LVTN/Dataset/demo/HR")
 
 d = ImageDraw.Draw(img)
 d.text((10, 10), "Hello World", fill=(255, 255, 0))
