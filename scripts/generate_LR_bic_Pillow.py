55db740fab40bad456831e72eec4f9979a653341
Tai <lythanhtaiyoung666@gmail.com>
2019-04-23 15:49:38 +0700

    fix mkdir to ensure path available

--- scripts/generate_LR_bic_Pillow.py
+++ scripts/generate_LR_bic_Pillow.py
@@ -2,6 +2,8 @@ from PIL import Image
 import sys
 import os
 
+from utils import util
+
 # import time
 from glob import glob
 
@@ -9,8 +11,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.progress_bar import ProgressBar  # noqa: E402
 
 # LR, HR path
-hr_path = "C:/Users/LTT/OneDrive - K3v/Documents/LVTN/dataset/demo/HR/*"
-lr_path = "C:/Users/LTT/OneDrive - K3v/Documents/LVTN/Dataset/demo/LRx4/"
+hr_path = "content/dataset/img_align_celeba_png_set_1_HR/*"
+lr_path = "content/dataset/img_align_celeba_png_set_1_LR/"
 hr_list = sorted(glob(hr_path))
 
 # Scale factor
@@ -18,6 +20,8 @@ ratio = 1 / 4
 # Coefficient
 a = -1 / 2
 pbar = ProgressBar(len(hr_list))
+
+util.mkdir(lr_path)
 
 for index, value in enumerate(hr_list):
     pbar.update("Read {}".format(value))
