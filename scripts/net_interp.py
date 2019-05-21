dd67980738e4c69ed746e9b2daf87930593197b1
Tai <lythanhtaiyoung666@gmail.com>
2019-05-21 19:47:57 +0700

    change dataset path

--- scripts/net_interp.py
+++ scripts/net_interp.py
@@ -3,9 +3,9 @@ from collections import OrderedDict
 
 alpha = 0.8
 
-net_PSNR_path = './models/RRDB_PSNR_x4.pth'
-net_ESRGAN_path = './models/RRDB_ESRGAN_x4.pth'
-net_interp_path = './models/interp_{:02d}.pth'.format(int(alpha*10))
+net_PSNR_path = '../experiments/RRDB_PSNR_x4/models/RRDB_PSNR_x4.pth'
+net_ESRGAN_path = '../experiments/RRDB_PSNR_x4/models/RRDB_ESRGAN_x4.pth'
+net_interp_path = '../experiments/RRDB_PSNR_x4/models/interp_{:02d}.pth'.format(int(alpha*10))
 
 net_PSNR = torch.load(net_PSNR_path)
 net_ESRGAN = torch.load(net_ESRGAN_path)
